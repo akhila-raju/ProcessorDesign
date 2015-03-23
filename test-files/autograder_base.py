1cb19785f1b05f0aebbdac8cc097f4c34c2a597e
Yuan Yuan <y.yuan@berkeley.edu>
2015-03-23 00:00:00 -0700

    cleaned up folder

--- test-files/autograder_base.py
+++ test-files/autograder_base.py
@@ -17,8 +17,6 @@ def student_reference_match_unbounded(student_out, reference_out):
   while True:
     line1 = student_out.readline()
     line2 = reference_out.readline()
-    print(line1)
-    print(line2)
     if line2 == '':
       break
     if line1 != line2:
@@ -175,4 +173,5 @@ def main(tests):
   else:
     print >> sys.stderr, "Incorrect usage!"
   sys.exit(1)
-    
+
+
